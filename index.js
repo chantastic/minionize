93796803395ba3341fcb5e0b387ca25de8fff2bd
Michael Chan <mijoch@gmail.com>
2017-02-23 10:58:46 -0800

    init: start somewhere

---
+++ index.js
@@ -0,0 +1,11 @@
+export default function (minionClassNames) {
+  return [
+    typeof minionClassNames === "object" && typeof minionClassNames.mn === "string" && minionClassNames.mn.split(" ").map(c => `${c}@mn`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.xs === "string" && minionClassNames.xs.split(" ").map(c => `${c}@xs`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.sm === "string" && minionClassNames.sm.split(" ").map(c => `${c}@sm`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.md === "string" && minionClassNames.md.split(" ").map(c => `${c}@md`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.lg === "string" && minionClassNames.lg.split(" ").map(c => `${c}@lg`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.xl === "string" && minionClassNames.xl.split(" ").map(c => `${c}@xl`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.mx === "string" && minionClassNames.mx.split(" ").map(c => `${c}@mx`).join(" "),
+  ].join(' ').trim()
+}
